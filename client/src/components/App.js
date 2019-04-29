6350d2f25b0823c737e653ea58a9c8273024d160
hirosoft40 <hirosoft40@gmail.com>
2019-04-28 20:45:35 -0500

    up to 284 complete

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,11 @@
+// ==============
+// - Switch of react-router-dom : shows only 1 routes
+// ==============
+
+
+
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import StreamCreate from "../components/streams/StreamCreate";
 import StreamEdit from "../components/streams/StreamEdit";
 import StreamDelete from "../components/streams/StreamDelete";
@@ -14,11 +20,13 @@ const App = () => {
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit/:id" component={StreamEdit} />
-          <Route path="/streams/delete/:id" component={StreamDelete} />
-          <Route path="/streams/show" component={StreamShow} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
